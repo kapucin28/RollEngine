85ed2bf460fd7380008f5b326bca44e061c5ba4d
Timbuli Remus <kapucin28@gmail.com>
2016-05-04 16:37:43 +0300

    Creating the RollEngine user interface

---
+++ src/main/RollUI.java
@@ -0,0 +1,14 @@
+package main;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * Created by TIMBULI REMUS K@puc!n on 04-May-16.
+ */
+public class RollUI extends Pane {
+
+    public RollUI() {
+
+    }
+
+}
