f3a0342ee7ef298cc32f48dc2a7051ed39c55ffd
Timbuli Remus <kapucin28@gmail.com>
2016-05-05 13:44:05 +0300

    Making style changes

--- src/main/RollMain.java
+++ src/main/RollMain.java
@@ -4,12 +4,9 @@ import alerts.ExitAlert;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 04-May-16.
@@ -18,7 +15,6 @@ public class RollMain extends Application {
 
     // Scene & Pane variables-------------------------------------------------------------------------------------------
     private final Pane pane = new Pane();
-    private final ImageView imageView = new ImageView("resources/redbackground.jpg");
     private final Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
     private final Scene scene = new Scene(pane, bounds.getWidth(), bounds.getHeight());
     //------------------------------------------------------------------------------------------------------------------
@@ -37,16 +33,11 @@ public class RollMain extends Application {
         //--------------------------------------------------------------------------------------------------------------
 
         // Adding RollUI to pane----------------------------------------------------------------------------------------
-        pane.getChildren().add(imageView);
-        imageView.setFitWidth(bounds.getWidth());
-        imageView.setFitHeight(bounds.getHeight());
         pane.getChildren().add(new RollUI());
         //--------------------------------------------------------------------------------------------------------------
 
         // Stage setup--------------------------------------------------------------------------------------------------
         stage.setTitle("RollEngine");
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
         stage.setOnCloseRequest(e -> {
