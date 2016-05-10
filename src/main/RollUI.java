b53e4835f4ebc305c8984eba53e4f1d27847adcc
Timbuli Remus <kapucin28@gmail.com>
2016-05-10 14:31:22 +0300

    Adding control panes to layout

--- src/main/RollUI.java
+++ src/main/RollUI.java
@@ -1,18 +1,25 @@
 package main;
 
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 04-May-16.
  */
 class RollUI extends Pane {
 
-    // Pane variables---------------------------------------------------------------------------------------------------
-    private final GridPane root = new GridPane();
-    //------------------------------------------------------------------------------------------------------------------
     // Constructing a web object----------------------------------------------------------------------------------------
     private Web web = new Web();
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Pane variables---------------------------------------------------------------------------------------------------
+    private final Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+    private final GridPane searchingGrid = new GridPane();
+    private final TabPane webPane = new TabPane();
+    private final GridPane root = new GridPane();
     //------------------------------------------------------------------------------------------------------------------
 
     // Constructor------------------------------------------------------------------------------------------------------
@@ -23,6 +30,12 @@ class RollUI extends Pane {
 
     // Layout setup method----------------------------------------------------------------------------------------------
     private void layoutSetup() {
+        searchingGrid.setPrefHeight(bounds.getHeight() - 700);
+        searchingGrid.setPrefWidth(bounds.getWidth());
+        webPane.setPrefHeight(bounds.getHeight() - 138);
+        webPane.setPrefWidth(bounds.getWidth() - 40);
+        root.add(searchingGrid, 0, 0);
+        root.add(webPane, 0, 1);
         getChildren().add(root);
     }
     //------------------------------------------------------------------------------------------------------------------
