138742ecae5cea71a3c3099bad9ba34bd1552a6e
kapucin28 <kapucin28@gmail.com>
2016-12-05 01:01:12 +0200

    Implementing interface

--- src/interfaces/ObjectTitles.java
+++ src/interfaces/ObjectTitles.java
@@ -9,5 +9,13 @@ public interface ObjectTitles {
 
     String cssFile = "style.css";
     String stageTitle = "RollEngine";
+    String exitButton = "Exit";
+    String newTab = "New Tab";
+    String historyTitle = "History";
+    String webProtocol = "https://www.";
+    String webDomain = ".com";
+    String defaultTabTitle = "Primary Tab";
+    String defaultSearchEngine = "https://www.google.com";
+    String spawnedTabTitle = "New Tab";
 
 }
